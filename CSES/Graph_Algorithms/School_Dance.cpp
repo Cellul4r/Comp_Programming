4f7a9f2dcf3ffd7fdf2937a23c69f58ee42545a2
Cellul4r <seriousme.com@gmail.com>
2024-11-27 20:52:20 +0700

    MAX-FLOW-27-11-2024

---
+++ CSES/Graph_Algorithms/School_Dance.cpp
@@ -0,0 +1,191 @@
+#include<bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '"' << x << '"';}
+void __print(const string &x) {cerr << '"' << x << '"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+//#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+//#else
+//#define dbg(x...)
+//#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep(i, a) for(int i=0;i<a;++i)
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+//#define f first
+//#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+const int N =1001;
+const int INF = 1e9+7;
+const long long LINF = 1e18+7;
+const int S = N;
+
+struct dinic {
+
+	struct edge {ll b, cap, flow, flip;};
+	vector<edge> g[S+2];
+    bool vis[S+2];
+    vector<pi> cut;
+    vector<vi> paths;
+
+	ll ans=0, d[S+2], ptr[S+2];
+	void add_edge (ll a, ll b, ll cap) {
+		g[a].push_back({b, cap, 0, g[b].size()});
+		g[b].push_back({a, 0, 0, g[a].size()-1});
+	}
+	ll dfs (ll u, ll flow=LLONG_MAX) {
+		if (u==S+1 || !flow) return flow;
+		while (++ptr[u] < g[u].size()) {
+			edge &e = g[u][ptr[u]];
+			if (d[e.b] != d[u]+1) continue;
+			if (ll pushed = dfs(e.b, min(flow, e.cap-e.flow))) {
+				e.flow += pushed;
+				g[e.b][e.flip].flow -= pushed;
+				return pushed;
+			}
+		}
+		return 0;
+    }
+
+    void findPath(int u, vi& path) {
+        if(u == S+1) {
+            paths.pb(path);
+            return;
+        }
+
+        trav(e, g[u]) {
+            if(e.flow > 0) {
+                path.pb(e.b);
+                e.flow = 0;
+                findPath(e.b, path);
+                path.pop_back();
+            }
+        }
+    }
+    void dfs2(int u) {
+        vis[u] = true;
+        trav(vw, g[u]) {
+            int v = vw.b;
+            if(vw.cap > vw.flow && !vis[v]) {
+                dfs2(v);
+            } 
+        }
+    }
+
+    void minCut() {
+        rep(i,S+2) {
+            if(!vis[i]) continue;
+            trav(v,g[i]) {
+                if(!vis[v.b] && v.cap > 0) {
+                    cut.pb(mp(i,v.b));
+                }
+            }
+        }
+    }
+
+	void calc() {
+		do {
+			vector<ll> q {S};
+			memset(d, 0, sizeof d);
+			ll i = -(d[S] = 1);
+			while (++i<q.size() && !d[S+1])
+				for (auto e: g[q[i]])
+					if (!d[e.b] && e.flow<e.cap) {
+						q.push_back(e.b);
+						d[e.b] = d[q[i]]+1;
+					}
+			memset(ptr, -1, sizeof ptr);
+			while(ll pushed=dfs(S)) ans+=pushed;
+		} while (d[S+1]);
+	}
+};
+
+void solve(){
+
+    dinic di;
+    int n,m,k;
+    cin>>n>>m>>k;
+    rep(i,k) {
+        int a,b;
+        cin>>a>>b;
+        di.add_edge(a,b+n,1);
+    }
+    FOR(i,1,n+1){
+        di.add_edge(S,i,1);
+    }
+    FOR(i,1,m+1) {
+        di.add_edge(i+n,S+1,1);
+    }
+    di.calc();
+    cout << di.ans << nl;
+    FOR(i,1,n+1){
+        trav(v, di.g[i]) {
+            if(v.flow == 1) {
+                cout << i << " " << v.b - n << nl;
+            }
+        }
+    }
+}
+
+int main(){
+   ios::sync_with_stdio(false);cin.tie(nullptr);
+   int t = 1;
+//    cin>>t;
+   while(t--)solve();
+}
---
+++ CSES/Graph_Algorithms/Teleporters_Path.cpp
@@ -0,0 +1,149 @@
+#include<bits/stdc++.h>
+using namespace std;
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '"' << x << '"';}
+void __print(const string &x) {cerr << '"' << x << '"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x);
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+//#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+//#else
+//#define dbg(x...)
+//#endif
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep(i, a) for(int i=0;i<a;++i)
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+//#define f first
+//#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const char nl = '\n';
+const int N =1e5+1;
+const int INF = 1e9+7;
+const long long LINF = 1e18+7;
+
+int indeg[N],outdeg[N];
+vi adj[N];
+
+void solve(){
+
+    int n,m;
+    cin>>n>>m;
+    rep(i,m) {
+        int u,v;
+        cin>>u>>v;
+        --u,--v;
+        indeg[v]++;
+        outdeg[u]++;
+        adj[u].pb(v);
+    }
+
+    bool flag = true;
+    if(outdeg[0] - indeg[0] != 1 || indeg[n-1] - outdeg[n-1] != 1) {
+        flag = false;
+    }
+    FOR(i,2,n-1){
+        if(indeg[i] != outdeg[i]) {
+            flag = false;
+            break;
+        }
+    }
+
+    // adj[n-1].pb(0);
+    // outdeg[n-1]++;
+    // indeg[0]++;
+
+    if(!flag) {
+        cout << "IMPOSSIBLE";
+        return;
+    }
+
+    stack<int> st;
+    vi curcuit;
+    st.push(0);
+    while(!st.empty()) {
+        int u = st.top();
+        if(outdeg[u]) {
+
+            int v = adj[u].back();
+            adj[u].pop_back();
+            --outdeg[u];
+            --indeg[v];
+            st.push(v);
+        } else {
+            curcuit.pb(u);
+            st.pop();
+        }
+    }
+
+    reverse(all(curcuit));  
+
+    if(sz(curcuit) != m+1) {
+        cout << "IMPOSSIBLE";
+        return;
+    }
+
+    trav(x,curcuit) {
+        cout << x + 1 << " ";
+    }
+}
+
+int main(){
+   ios::sync_with_stdio(false);cin.tie(nullptr);
+   int t = 1;
+//    cin>>t;
+   while(t--)solve();
+}
